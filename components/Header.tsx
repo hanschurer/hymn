3989127305a4119c22900cb2d1a91086c193b9e9
Han Wang <78269193+hanwangxxx@users.noreply.github.com>
2023-11-05 16:19:25 +0800

    feat: reveal home nav

--- components/Header.tsx
+++ components/Header.tsx
@@ -29,17 +29,15 @@ const Header = () => {
         </Link>
       </div>
       <div className="flex items-center space-x-4 leading-5 sm:space-x-6">
-        {headerNavLinks
-          .filter((link) => link.href !== '/')
-          .map((link) => (
-            <Link
-              key={link.title}
-              href={link.href}
-              className="hidden font-medium text-gray-900 dark:text-gray-100 sm:block"
-            >
-              {link.title}
-            </Link>
-          ))}
+        {headerNavLinks.map((link) => (
+          <Link
+            key={link.title}
+            href={link.href}
+            className="hidden font-medium text-gray-900 dark:text-gray-100 sm:block"
+          >
+            {link.title}
+          </Link>
+        ))}
         <SearchButton />
         <ThemeSwitch />
         <MobileNav />
