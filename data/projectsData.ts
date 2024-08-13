5df81595f1055bcdada45f4225c9c96daa2cee89
Han Wang <hanwangx@gmail.com>
2024-08-13 17:19:37 +0800

    Update projectsData.ts

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,19 +1,17 @@
 const projectsData = [
   {
     title: 'Football AI Game',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
+    description: `This a soccer AI game made in Unity. There are three modes in the game. The first one is a rule-based AI mode implemented with a decision tree strategy. The second is an AI trained in Machine Learning and the third is a human player cheating mode.`,
+    imgSrc:
+      'https://res.cloudinary.com/marcomontalbano/image/upload/v1723539408/video_to_markdown/images/youtube--4LsVB76mdgE-c05b58ac6eb4c4700831b2b3070cd403.jpg',
+    href: '/blog/game/soccer-AI-game',
   },
   {
     title: 'Zoombie Stealth',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    description: `This game is all about a third-person stealth game which the final goal is to reach the treasure.`,
+    imgSrc:
+      '![](https://raw.githubusercontent.com/hanschurer/picgo/master/img/Screenshot%202024-08-12%20at%2022.27.38.png)',
+    href: '/blog/game/zombie-stealth',
   },
   {
     title: 'Four in one, board AI game',
